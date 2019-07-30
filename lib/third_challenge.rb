adccb407b73c26c9e329f3b2aaef30a58d922405
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 17:09:17 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,12 +2,12 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => ["Lord Montague"],
-        :age => ["53"]
+        :name => "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
-        :name => ["Lady Montague"],
-        :age => ["54"]
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
         :name => ["Romeo"],
